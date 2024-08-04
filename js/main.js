735f993e8f2aac4a0260ce5834c42d7c1b5aa383
Vyacheslav Byrdaev <smit467bs@gmail.com>
2024-08-04 14:19:45 +0300

    feat: add effects cards js

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 import '../css/style.css';
 
 
-// document.addEventListener('mousemove', (e) => {
-//   const container = document.querySelector('.__container');
-//   const cv = document.querySelector('.__cv');
-//   const rect = container.getBoundingClientRect();
-//   const x = e.clientX - rect.left;
-//   const y = e.clientY - rect.top;
-//
-//   const centerX = rect.width / 2;
-//   const centerY = rect.height / 2;
-//
-//   const deltaX = (x - centerX) / centerX;
-//   const deltaY = (y - centerY) / centerY;
-//
-//   const maxAngle = 15;
-//
-//   const rotateY = deltaX * maxAngle;
-//   const rotateX = -deltaY * maxAngle;
-//
-//   cv.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
-// });
-//
+document.addEventListener('mousemove', (e) => {
+  const container = document.querySelector('.__container');
+  const cv = document.querySelector('.__cv');
+  const rect = container.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  const centerX = rect.width / 2;
+  const centerY = rect.height / 2;
+
+  const deltaX = (x - centerX) / centerX;
+  const deltaY = (y - centerY) / centerY;
+
+  const maxAngle = 15;
+
+  const rotateY = deltaX * maxAngle;
+  const rotateX = -deltaY * maxAngle;
+
+  cv.style.transform = `rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
+});
+
